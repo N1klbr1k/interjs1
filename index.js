4e15665c997fd9c3f34a0b5a3cadcca8a69c05d7
N1klbr1k <lemmonynikit@gmail.com>
2022-04-06 15:18:18 -0600

    A commit for completion of core lab

---
+++ index.js
@@ -0,0 +1,40 @@
+// console.log('hello world')
+
+const message = document.querySelector('#message');
+
+
+const addMovie = (event) => {
+    event.preventDefault();
+    const inputField = document.querySelector('input')
+    let movie = document.createElement('li')
+    let movieTitle = document.createElement('span')
+//------------------------------
+    movieTitle.textContent = inputField.value;
+//--------------------------------
+    movieTitle.addEventListener('click', crossOffMovie)
+    movie.appendChild(movieTitle);
+//----------------------------------
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn);
+    document.querySelector('ul').appendChild(movie);
+    inputField.value = ' ';
+}
+
+const deleteMovie = event => {
+event.target.parentNode.remove()
+message.textContent = 'Movie deleted';
+}
+const crossOffMovie = event => {
+    event.target.classList.toggle('checked')
+   if( event.target.classList.contains('checked')){
+   message.textContent = 'Movie Watched!'
+   } else {
+       message.textContent = ' Movie added back!'
+   }
+}
+
+const newForm = document.querySelector('form')
+
+newForm.addEventListener('submit', addMovie)
